35248df8112cbeab72c213e4cf88513f4ec77d3c
Natalia Potocekova <57913414+potocekn@users.noreply.github.com>
2020-10-15 10:49:08 +0200

    Add files via upload

---
+++ player_tft.py
@@ -0,0 +1,28 @@
+import random
+
+class MyPlayer:
+    '''Hrac hraje na zaklade nahodneho vyberu'''
+    def __init__(self, payoff_matrix, number_of_iterations=100):
+        self.payoff_matrix = payoff_matrix
+        self.number_of_iterations=number_of_iterations
+        self.history = [None]*self.number_of_iterations
+
+    def move(self):
+        if (self.history[0][1] != True) and (self.history[0][1] != False):  # first time always cooperates
+            return False
+        else:
+            if self.history[0][1] == False:
+                return False
+            if self.history[0][1] == True:
+                return True
+
+
+    def record_last_moves(self, my_last_move, opponent_last_move):
+        # INPUTS: my_last_move, opponent_last_move
+                    # respective last moves of both players
+        # OUTPUTS: none
+        # notes down players' moves in pairs in a list
+        del self.history[-1]
+        self.history.insert(0, (my_last_move, opponent_last_move))
+
+
